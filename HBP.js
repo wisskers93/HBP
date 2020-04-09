c066f8ded6d9a03194f5b19be13a0e053811c75d
Wisskers93 <cmwgonzales18@gmail.com>
2020-04-09 13:12:11 -0400

    created playerFunction

--- HBP.js
+++ HBP.js
@@ -1,6 +1,26 @@
 const button1 = document.querySelector("#button1");
+button1.addEventListener("click", playerFunction(1));
+
 const button2 = document.querySelector("#button2");
+button2.addEventListener("click", playerFunction(2));
+
 const button3 = document.querySelector("#button3");
+button3.addEventListener("click", playerFunction(3));
+
+
+function playerFunction(selectedChar){
+  let playerOne;
+
+  if(selectedChar =1){
+    playerOne="hunter";
+    } else if(selectedChar =2){
+    playerOne="bear";
+    }else {
+    playerOne="princess";
+    } 
+}
+
+
 
 let selection = index.html;
 let i;
@@ -9,20 +29,10 @@ let value1;
 let value2;
 
 
-button1.addEventListener("click", hunterFunction()) {
     let player = 0
     value1="hunter";
         let robot = buttons[i.getRandomInt(3)].selection;
-        if(i =0){
-        robot=0;
-        value2="hunter";
-        } else if(i=1){
-        robot=1;
-        value2="bear";
-        }else if(i=2){
-        robot=2;
-        value2="princess";
-        } 
+        
 
     
     if (player == robot) {
@@ -42,65 +52,6 @@ button1.addEventListener("click", hunterFunction()) {
         ${result}`;
   });
     
-button2.addEventListener("click", bearFunction()) {
-    let player = 1
-    value1="bear";
-        let robot = buttons[i.getRandomInt(3)].selection;
-        if(i =0){
-        robot=0;
-        value2="hunter";
-        } else if(i=1){
-        robot=1;
-        value2="bear";
-        }else if(i=2){
-        robot=2;
-        value2="princess";
-        } 
-
-    
-    if (player == robot) {
-      result = "Tie!";
-    }
-    else if ((player == "hunter" && robot == "bear") || (player == "bear" && robot == "princess") || (player == "princess" && robot == "hunter")) {
-      result = "You Won!";
-    }
-    
-    else {
-      result = "You Lost!";
-    }
-
-    document.querySelector(".result").innerHTML = 
-      ` ${value1} 
-        ${value2} </br></br>
-        ${result}`;
-  });
-button1.addEventListener("click", princessFunction()) {
-    let player = 2
-    value1="princess";
-        let robot = buttons[i.getRandomInt(3)].selection;
-        if(i =0){
-        robot=0;
-        value2="hunter";
-        } else if(i=1){
-        robot=1;
-        value2="bear";
-        }else if(i=2){
-        robot=2;
-        value2="princess";
-        } 
-
-    
-    if (player == robot) {
-      result = "Tie!";
-    }
-    else if ((player == "hunter" && robot == "bear") || (player == "bear" && robot == "princess") || (player == "princess" && robot == "hunter")) {
-      result = "You Won!";
-    }
-    
-    else {
-      result = "You Lost!";
-    }
-
     document.querySelector(".result").innerHTML = 
       ` ${value1} 
         ${value2} </br></br>
