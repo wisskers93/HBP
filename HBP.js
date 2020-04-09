3b904a90130884d6e1d404bcc8a6d65304eb37fd
Wisskers93 <cmwgonzales18@gmail.com>
2020-04-09 14:33:40 -0400

    updated event listener

--- HBP.js
+++ HBP.js
@@ -1,62 +1,44 @@
-const button1 = document.querySelector("#button1");
-button1.addEventListener("click", playerFunction(1));
 
-const button2 = document.querySelector("#button2");
-button2.addEventListener("click", playerFunction(2));
-
-const button3 = document.querySelector("#button3");
-button3.addEventListener("click", playerFunction(3));
+document.getElementById("button1").addEventListener("click", () => { resultsPage(); playerFunction(1);});
+document.getElementById("button2").addEventListener("click", () => { resultsPage(); playerFunction(2);});
+document.getElementById("button3").addEventListener("click", () => { resultsPage(); playerFunction(3);});
 
 
-function playerFunction(selectedChar){
-  let playerOne;
-
-  if(selectedChar =1){
-    playerOne="hunter";
-    } else if(selectedChar =2){
-    playerOne="bear";
-    }else {
-    playerOne="princess";
-    } 
+function resultsPage() { 
+  document.getElementById("mainFrameOne").style.display="none"; 
+  document.getElementById("headertext1").style.display="none";
+  document.getElementById("mainFrameTwo").style.display="block"; 
+  document.getElementById("headertext2").style.display="block";
 }
 
 
 
-let selection = index.html;
-let i;
-let result;
-let value1;
-let value2;
+function playerFunction(selectedChar){
+
+  let player= selectedChar;
+  let robot = Math.random()*3; 
+    
+  if (player == robot) {
+    result = "Tie!";
+  }
+  else if ((player == 1 && robot == 2) || (player == 2 && robot == 3) || (player == 3 && robot == 1)) {
+    result = "You Won!";
+  }
+  
+  else {
+    result = "You Lost!";
+  }
+
+  document.querySelector(".result").innerHTML = 
+  ` ${player} 
+    ${robot} </br></br>
+    ${result}`;
 
 
-    let player = 0
-    value1="hunter";
-        let robot = buttons[i.getRandomInt(3)].selection;
-        
+}
 
     
-    if (player == robot) {
-      result = "Tie!";
-    }
-    else if ((player == "hunter" && robot == "bear") || (player == "bear" && robot == "princess") || (player == "princess" && robot == "hunter")) {
-      result = "You Won!";
-    }
-    
-    else {
-      result = "You Lost!";
-    }
-
-    document.querySelector(".result").innerHTML = 
-     ` ${value1} 
-        ${value2} </br></br>
-        ${result}`;
-  });
-    
-    document.querySelector(".result").innerHTML = 
-      ` ${value1} 
-        ${value2} </br></br>
-        ${result}`;
-  });
+ 
     
     
 
