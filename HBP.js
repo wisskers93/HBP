9a3db6a95f62cf8cd18eeca7652935422bd45010
Wisskers93 <cmwgonzales18@gmail.com>
2020-04-10 11:45:19 -0400

    fixed princess img - need to fix results

--- HBP.js
+++ HBP.js
@@ -4,6 +4,11 @@
 let button1 = document.querySelector("#button1");
 button1.addEventListener("click", resultsPage1);
 
+let button2 = document.querySelector("#button2");
+button2.addEventListener("click", resultsPage2);
+
+let button3 = document.querySelector("#button3");
+button3.addEventListener("click", resultsPage3);
 
 let revert = document.querySelector("#revert");
 revert.addEventListener("click", revertPage);
@@ -32,13 +37,61 @@ function resultsPage1() {
         break;
       case(3):
         result = "You Lost!"
-        document.getElementById("imgRobot").src = "src imgs\pixel-art-princess-vector-19072212.jpg";
+        document.getElementById("imgRobot").src = "http://pixelartmaker.com/art/7a40d068e7d71de.png";
         break;
     }
+}
+
+function resultsPage2() { 
+  document.getElementById("mainFrameOne").style.display="none"; 
+  document.getElementById("headertext1").style.display="none";
+  document.getElementById("mainFrameTwo").style.display="block"; 
+  document.getElementById("revert").style.display="block";
 
   
+  document.getElementById("imgPlayer").src = "http://pixelartmaker.com/art/d2dc9caaef73f10.png";
   
+  let robot = Math.floor(Math.random()*3)+1; 
+    switch(robot){
+      case(1):
+        result = "You Lost!";
+        document.getElementById("imgRobot").src = "https://previews.123rf.com/images/saphatthachat/saphatthachat1805/saphatthachat180500171/102007681-vector-pixel-art-hunter-isolated-cartoon.jpg";
+        break;
+      case(2):
+        result = "Tie!";
+        document.getElementById("imgRobot").src = "http://pixelartmaker.com/art/d2dc9caaef73f10.png";
+        break;
+      case(3):
+        result = "You Won!"
+        document.getElementById("imgRobot").src = "http://pixelartmaker.com/art/7a40d068e7d71de.png";
+        break;
+    }
+}
 
+function resultsPage3() { 
+  document.getElementById("mainFrameOne").style.display="none"; 
+  document.getElementById("headertext1").style.display="none";
+  document.getElementById("mainFrameTwo").style.display="block"; 
+  document.getElementById("revert").style.display="block";
+
+  
+  document.getElementById("imgPlayer").src = "http://pixelartmaker.com/art/7a40d068e7d71de.png";
+  
+  let robot = Math.floor(Math.random()*3)+1; 
+    switch(robot){
+      case(1):
+        result = "You Won!";
+        document.getElementById("imgRobot").src = "https://previews.123rf.com/images/saphatthachat/saphatthachat1805/saphatthachat180500171/102007681-vector-pixel-art-hunter-isolated-cartoon.jpg";
+        break;
+      case(2):
+        result = "You Lost!";
+        document.getElementById("imgRobot").src = "http://pixelartmaker.com/art/d2dc9caaef73f10.png";
+        break;
+      case(3):
+        result = "Tie!"
+        document.getElementById("imgRobot").src = "http://pixelartmaker.com/art/7a40d068e7d71de.png";
+        break;
+    }
 }
 
 
