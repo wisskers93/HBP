b7d8f4939fba6caf2a1e34f8d6c1bf71429e577d
Wisskers93 <cmwgonzales18@gmail.com>
2020-04-10 11:02:14 -0400

    finally works!!! fix return needed

--- HBP.js
+++ HBP.js
@@ -1,41 +1,57 @@
 
-document.getElementById("button1").addEventListener("click", () => { resultsPage(); playerFunction(1);});
-document.getElementById("button2").addEventListener("click", () => { resultsPage(); playerFunction(2);});
-document.getElementById("button3").addEventListener("click", () => { resultsPage(); playerFunction(3);});
 
 
-function resultsPage() { 
+let button1 = document.querySelector("#button1");
+button1.addEventListener("click", resultsPage1);
+
+
+let revert = document.querySelector("#revert");
+revert.addEventListener("click", revertPage);
+
+
+
+
+function resultsPage1() { 
   document.getElementById("mainFrameOne").style.display="none"; 
   document.getElementById("headertext1").style.display="none";
   document.getElementById("mainFrameTwo").style.display="block"; 
-  document.getElementById("headertext2").style.display="block";
-}
+  document.getElementById("revert").style.display="block";
 
-
-
-function playerFunction(selectedChar){
-
-  let player= selectedChar;
-  let robot = Math.random()*3; 
-    
-  if (player == robot) {
-    result = "Tie!";
-  }
-  else if ((player == 1 && robot == 2) || (player == 2 && robot == 3) || (player == 3 && robot == 1)) {
-    result = "You Won!";
-  }
   
-  else {
-    result = "You Lost!";
-  }
+  document.getElementById("imgPlayer").src = "https://previews.123rf.com/images/saphatthachat/saphatthachat1805/saphatthachat180500171/102007681-vector-pixel-art-hunter-isolated-cartoon.jpg";
+  
+  let robot = Math.floor(Math.random()*3)+1; 
+    switch(robot){
+      case(1):
+        result = "Tie!";
+        document.getElementById("imgRobot").src = "https://previews.123rf.com/images/saphatthachat/saphatthachat1805/saphatthachat180500171/102007681-vector-pixel-art-hunter-isolated-cartoon.jpg";
+        break;
+      case(2):
+        result = "You Won!";
+        document.getElementById("imgRobot").src = "http://pixelartmaker.com/art/d2dc9caaef73f10.png";
+        break;
+      case(3):
+        result = "You Lost!"
+        document.getElementById("imgRobot").src = "src imgs\pixel-art-princess-vector-19072212.jpg";
+        break;
+    }
 
-  document.querySelector(".result").innerHTML = 
-  ` ${player} 
-    ${robot} </br></br>
-    ${result}`;
-
+  
+  
 
 }
+
+
+function revertPage() { 
+  document.getElementById("mainFrameOne").style.display="inline-flex"; 
+  document.getElementById("headertext1").style.display="block";
+  document.getElementById("mainFrameTwo").style.display="none"; 
+  document.getElementById("revert").style.display="none";
+}
+
+
+
+
 
     
  
